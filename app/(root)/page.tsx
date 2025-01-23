24e28c31ace210efb76ec80721aa1283649d68ad
adilike <adilike_francis@yahoo.com>
2025-01-23 22:01:20 +0100

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,19 @@
+import ProductList from '@/components/shared/product/product-list';
+//import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
+
+//export default async function Home() {
+  const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList
+        data={latestProducts}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
+}
+
+export default Homepage;
